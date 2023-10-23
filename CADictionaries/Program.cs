b0934888c02e49a834f21a15b8b2e4792bf7f9ce
ramsalo1981 <rami.salloum@hotmail.com>
2023-10-23 17:15:46 +0200

    first commit

---
+++ CADictionaries/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace CADictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var article =
+            //    "Dot NET is a free cross-platform and open source developer platform" +
+            //    "for building many different types of applications" +
+            //    "With Dot NET you can use multiple languages and libraries" +
+            //    "to build for web and IoT";
+            //// i want to make every letter key 
+            //// key:'d' value : Dot developer ...
+            //Dictionary<char, List<string>> lettersDictionary = new Dictionary<char, List<string>>();
+            //foreach (var word in article.Split())
+            //{
+            //    //Console.WriteLine(word);
+            //    foreach (var ch in word)
+            //    {
+            //        char c = Char.ToLower(ch);
+            //        if (!lettersDictionary.ContainsKey(c))
+            //        {
+            //            lettersDictionary.Add(c, new List<string> { word });
+            //        }
+            //        else
+            //        {
+            //            lettersDictionary[c].Add(word);
+            //        }
+
+            //    }
+
+            //}
+            //foreach (var entry in lettersDictionary)
+            //{
+            //    Console.Write($"'{entry.Key}': ");
+            //    foreach (var word in entry.Value)
+            //    {
+            //        Console.WriteLine($"\t\t \"{word}\"");
+            //    }
+            //}
+            var emps = new List<Employee>
+            {
+                new Employee {Id = 100, Name = "Reem S.", ReportTo = null},
+                new Employee {Id = 101, Name = "Raed M.", ReportTo = 100 },
+                new Employee {Id = 102, Name = "Ali B.", ReportTo = 100 },
+                new Employee {Id = 103, Name = "Abeer S.", ReportTo = 102 },
+                new Employee {Id = 104, Name = "Radwan N.", ReportTo = 102 },
+                new Employee {Id = 105, Name = "Nancy R.", ReportTo = 101 },
+                new Employee {Id = 106, Name = "Saleh A.", ReportTo = 104 }
+            };
+            //var managers1 = emps.ToLookup(y => y.ReportTo).ToDictionary(y => y.Key ?? -1, y => y.ToList());
+            //foreach (var employee in managers1)
+            //{
+            //    Console.WriteLine(employee.Key);
+            //}
+            //Console.ReadKey();
+            var managers = emps.GroupBy(x => x.ReportTo).ToDictionary(x => x.Key ?? -1, x => x.ToList());
+            //foreach (var employee in managers)
+            //{
+            //    if (employee.Key == -1)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(employee.Key);
+            //    foreach (var item in employee.Value)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            foreach (var entry in managers)
+            {
+                if (entry.Key == -1)
+                    continue;
+                var manager = emps.FirstOrDefault(x => x.Id == entry.Key);
+
+                Console.WriteLine($"{manager}");
+
+                foreach (var emp in entry.Value)
+                {
+                    Console.WriteLine($"\t\t\"{emp}\"");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? ReportTo { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{Id}] {Name}";
+        }
+    }
+}
---
+++ CALists/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace CALists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var egypt = new Country { ISOCode = "EGY", Name = "Egypt" };
+            var jordan = new Country { ISOCode = "JOR", Name = "Jordan" };
+            var iraq = new Country { ISOCode = "IRQ", Name = "Iraq" };
+
+            Country[] countriesArray = new Country[] { egypt, jordan, iraq };
+
+
+            // Constructors
+
+            //List<Country> countries = new List<Country>();
+            //List<Country> countries = new List<Country>(3);
+            List<Country> countries = new List<Country>(countriesArray);
+            Print(countries);
+            Console.WriteLine("----------------");
+            //Methods
+            countries.Add(new Country { ISOCode = "BRA", Name = "Brasil" });
+            Print(countries);
+            Console.WriteLine("----------------");
+
+            //countries.AddRange(countriesArray);
+            //Print(countries);
+            //Console.WriteLine("----------------");
+
+            countries.Insert(1, new Country { ISOCode = "FRA", Name = "France" });//Insert[index,object]
+            Print(countries);
+            Console.WriteLine("----------------");
+            //remove an element
+            countries.RemoveAt(4);
+            Print(countries);
+            Console.WriteLine("----------------");
+            //remove all elements end with ce
+            countries.RemoveAll(x => x.Name.EndsWith("ce"));
+            Print(countries);
+            Console.WriteLine("----------------");
+            //remove an object
+            //countries.Remove(iraq);
+            //------------------------------(1)------------------------
+            countries.Remove(new Country { ISOCode = "IRQ", Name = "Iraq" });// we use equal and gethush for delete it (see 1)
+            Print(countries);
+
+
+
+
+            Console.ReadKey();
+        }
+        static void Print(List<Country> countries)
+        {
+            foreach (var c in countries)
+            {
+                Console.WriteLine(c);
+            }
+
+            // Properties 
+            Console.WriteLine($"Count: {countries.Count}"); // actual count
+            Console.WriteLine($"Capacity: {countries.Capacity}"); // initial capacity for inner data structure
+        }
+    }
+    class Country
+    {
+        public string ISOCode { get; set; }
+        public string Name { get; set; }
+
+        //---------------(1)--------------------
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 19;
+                hash = hash * 23 + ISOCode.GetHashCode();
+                hash = hash * 23 +Name.GetHashCode();
+                return hash;
+            }
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var country = obj as Country;
+            if(country is null) return false;
+            return this.ISOCode.Equals(country.ISOCode, StringComparison.OrdinalIgnoreCase) 
+                && this.Name.Equals(country.Name, StringComparison.OrdinalIgnoreCase);
+            
+        }
+        //----------------------------------------------------------------------------------
+        public override string ToString()
+        {
+            return $"{Name} ({ISOCode})"; // Egypt (EGY)
+        }
+    }
+}
